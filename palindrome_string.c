d1d5c8bf65fd84f8fb7767f47add76bab2c54e11
Tanvi Muley <tanvimuley27@gmail.com>
2024-12-03 22:09:46 +0530

    Create palindrome_string.c

---
+++ palindrome_string.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char word[10];
+   int i, j, end, f=1;
+   printf("Enter the word: ");
+   scanf("%s",&word);
+   end=strlen(word);
+   i=0; 
+   j=end-1;
+   
+   while(i<j)
+   {
+       if(word[i] != word[j])
+       {
+           f=0;
+           break;
+       }
+       i++;
+       j--;
+   }
+   
+   if(f==1)
+       printf("\nThe word is a palindrome");
+   else
+       printf("\nThe word is not a palindrome");
+  
+   return 0;
+}
+/* Output 1-
+    Enter the word: madam
+
+    The word is a palindrome
+  Output 2-
+    Enter the word: hello
+
+    The word is not a palindrome */
+    
+  
